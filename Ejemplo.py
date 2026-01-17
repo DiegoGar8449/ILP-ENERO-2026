3a28c74272269bd9d81cb2252d74634b8eab5324
Diego García Rodríguez <diegogarcia102010@hotmail.com>
2026-01-17 11:41:58 -0600

    Primera subida del archivo de ejemplo Python
    En el curso practicamos la función print y operadores aritméticos para realizar algunos ejemplos con las reglas sintácticas de Python

---
+++ Ejemplo.py
@@ -0,0 +1,12 @@
+#Estos son ejemplos vistos en la primera clase
+#Con Python, se pone un gato para escribir un comentario.
+
+print("Diego"); #Nombre
+print('Garcia'); #Primer apellido
+print('Rodriguez'); #Segundo apellido
+
+print(); #Funcion vacia, para dar espacio
+print("Suma = ", 5+2); #Suma y Concatenacion de suma
+print("Resta = ", 8-7); #Resta y concatenacion de resta
+print("Division = ", 16/2); #Division y concatenacion de division
+print("Multi = ", 6*62); #Multiplicacion y concatenacion de division
