c4dd302c0ae5031f7e278f3ed64ab37449dc030e
Diego García Rodríguez <diegogarcia102010@hotmail.com>
2026-01-23 22:14:56 -0600

    Ejemplo 2 con Potencia y Mod
    Se solicitó implementar las operaciones: Potencia, raiz cuadrada, raiz cubica y mod, pero solo pude implementar potencia y mod

--- Ejemplo 2.py
+++ Ejemplo 2.py
@@ -5,16 +5,18 @@
 num1 = float(input("Ingrese el primer numero: "))
 num2 = float(input("Ingrese el segundo numero: "))
 
-
-
 #Procesos (Calculos u operaciones matematicas y/o logicas)
 suma = num1 + num2
 resta = num1 - num2
 multiplicacion = num1*num2
 division = num1/num2
+potencia = num1**num2
+mod = num1%num2
 
 #Salida de datos (Resultados)
 print("El resultado de la suma es: ", suma); #Concatenacion de la suma
 print("El resultado de la resta es: ", resta); #Concatenacion de la resta
 print("El resultado de la multiplicacion es: " + str(multiplicacion)); # + str(variable) esto es casteo, convertir un tipo de dato en otro
-print(f"division = {division}"); #Interpolacion de cadenas
+print(f"El resultado de la division es = {division}"); #Interpolacion de cadenas
+print("El resultado de la potencia es: ",potencia);
+print("El resultado del mod es: ", mod);
