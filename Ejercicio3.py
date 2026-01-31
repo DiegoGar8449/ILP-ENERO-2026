3dae5197bd570319b72eb0b644ef236c701550a3
Diego García Rodríguez <diegogarcia102010@hotmail.com>
2026-01-31 13:19:25 -0600

    Ejercicio 3
    Se resta el año actual menos el año de nacimiento para calcular la edad de la persona

---
+++ Ejercicio3.py
@@ -0,0 +1,11 @@
+#3. Determinar la edad de una persona conociendo el año actual y el año de nacimiento.
+
+#Entrada de datos
+nacimiento = int(input("Ingrese el año de su nacimiento: "))
+actual = int(input("Ingrese el año actual: "))
+
+#Proceso
+edad = actual-nacimiento
+
+#Salida de datos
+print("Su edad es: ",edad);
