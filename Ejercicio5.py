cab8a4eb66a27b6b6c8e336016f561647c8ec7fd
Diego García Rodríguez <diegogarcia102010@hotmail.com>
2026-01-31 13:20:34 -0600

    Ejercicio 5 Raíz cuadrada
    No me fue posible acomodar las operaciones para que se realizara la operación

---
+++ Ejercicio5.py
@@ -0,0 +1,12 @@
+#5. Obtener valores para: a, b y c. Calcular la fórmula general.
+import math
+#Entrada de datos
+a = float(input("Ingrese el valor de a: "))
+b = float(input("Ingrese el valor de b: "))
+c = float(input("Ingrese el valor de c: "))
+
+#Proceso de datos
+x1 = (-b-(pow(b**2(-4*a*c,1/2))))/2*a
+
+#Salida de datos
+print("x1 es igual a: ",x1)
